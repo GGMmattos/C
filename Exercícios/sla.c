cdd0c81dd06a4bfb4b13357d3bc983fd84fb3ca5
GGM <ra116843@uem.br>
2021-03-25 13:25:13 -0300

    ATT

---
+++ Exercícios/sla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+ 
+int n1,n2;
+int total = 0;
+
+printf("Informe um numero: ");
+scanf("%d",&n1);
+
+printf("Informe outro numero: ");
+scanf("%d",&n2);
+        
+
+
+total = n1+n2;
+
+printf("A soma dos dois numeros informado é igual a %d:",n1);
+}
